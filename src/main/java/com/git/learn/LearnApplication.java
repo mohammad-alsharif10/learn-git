ff21e9e909a3db959a9d6de6e1e84a0acff03949
mohammad <malsharif@kn-it.com>
2020-03-01 11:15:40 +0200

    commiting the application runner

--- src/main/java/com/git/learn/LearnApplication.java
+++ src/main/java/com/git/learn/LearnApplication.java
@@ -1,13 +1,20 @@
 package com.git.learn;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnApplication {
+public class LearnApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
     }
 
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
